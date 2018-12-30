028f0ff681d5fe430cba13a9011cf3afd73bc237
syzhang <hefeizsy@163.com>
2018-12-30 00:01:41 -0500

    Update generate_vocabulary.py

--- generate_vocabulary.py
+++ generate_vocabulary.py
@@ -7,6 +7,13 @@ def word_tokenize(s):
 
 if __name__ == '__main__':
 
+    glove_dim = 300
+    glove_path = '/localdisk/szhang83/Developer/LocalizingMoments/data/glove.6B.%dd.txt' % glove_dim
+    glove_txt = open(glove_path).readlines()
+    glove_txt = [g.strip() for g in glove_txt]
+    glove_vector = [g.split(' ') for g in glove_txt]
+    glove_words = [g[0] for g in glove_vector]
+
     txt_path = 'exp_data/Charades_STA/vocab_glove_complete.txt'
     vocab_file = open(txt_path, 'w')
     words = []
@@ -17,6 +24,7 @@ if __name__ == '__main__':
             info, sentence = anno.split('##')
             sentence = sentence.split('.\n')[0]
             words.extend(word_tokenize(sentence))
-    vocab_file.write('\n'.join(set(words)))
+    valid_words = list(filter(lambda w: w in glove_words, set(words)))
+    vocab_file.write('\n'.join(set(valid_words)))
 
 
